4bfe42155e131532eb02aca062ceaf6fee2429d3
Sundze <keyonmwesundze@mail.com>
2025-01-18 19:13:47 +0100

    line charts

--- src/components/back-office/charts/WeeklySalesCharts.jsx
+++ src/components/back-office/charts/WeeklySalesCharts.jsx
@@ -1,7 +1,70 @@
 "use client";
 import { useState } from "react";
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import { faker } from "@faker-js/faker";
 
 export default function WeeklySalesCharts() {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: "top",
+      },
+      title: {
+        display: false,
+        text: "Chart.js Line Chart",
+      },
+    },
+  };
+
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: labels.map(() => faker.number.int({ min: -1000, max: 1000 })),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
+
   const tabs = [
     {
       title: "Sales",
@@ -42,7 +105,7 @@ export default function WeeklySalesCharts() {
         {/* Content to display */}
         {tabs.map((tab, i) => {
           if (chartsToDisplay === tab.type) {
-            return <h2>{tab.title}</h2>;
+            return <Line options={options} data={data} />;
           }
           return null;
         })}
