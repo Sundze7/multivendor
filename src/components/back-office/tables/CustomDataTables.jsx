49d12252da0e72a97b5fff8eba700714ea0794fe
Sundze <keyonmwesundze@mail.com>
2025-01-21 19:52:44 +0100

    pagination

--- src/components/back-office/tables/CustomDataTables.jsx
+++ src/components/back-office/tables/CustomDataTables.jsx
@@ -1,4 +1,15 @@
+"use client";
+import { useState } from "react";
+import data from "../../../../data.json";
 export default function CustomDataTables() {
+  const PAGE_SIZE = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+  const startIndex = [currentPage - 1] * PAGE_SIZE;
+  const endIndex = startIndex + PAGE_SIZE;
+  const currentlyDisplayedData = data.slice(startIndex, endIndex);
+  const totalPages = Math.ceil(data.length / PAGE_SIZE);
+
+  function handlesPageChange(page) {}
   return (
     <div>
       <h2 className="text-xl font-bold mb-4">Recent Orders</h2>
@@ -22,54 +33,70 @@ export default function CustomDataTables() {
                   </div>
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Product name
+                  ID
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Color
+                  First Name
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Category
+                  Last Name
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Price
+                  Email
                 </th>
                 <th scope="col" className="px-6 py-3">
-                  Action
+                  Gender
                 </th>
               </tr>
             </thead>
             <tbody>
-              <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td className="w-4 p-4">
-                  <div className="flex items-center">
-                    <input
-                      id="checkbox-table-search-1"
-                      type="checkbox"
-                      className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label for="checkbox-table-search-1" className="sr-only">
-                      checkbox
-                    </label>
-                  </div>
-                </td>
-                <th
-                  scope="row"
-                  className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
-                >
-                  Apple MacBook Pro 17"
-                </th>
-                <td className="px-6 py-4">Silver</td>
-                <td className="px-6 py-4">Laptop</td>
-                <td className="px-6 py-4">$2999</td>
-                <td className="px-6 py-4">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+              {currentlyDisplayedData.map((item, i) => {
+                return (
+                  <tr
+                    key={i}
+                    className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200 hover:bg-gray-50 dark:hover:bg-gray-600"
                   >
-                    Edit
-                  </a>
-                </td>
-              </tr>
+                    <td className="w-4 p-4">
+                      <div className="flex items-center">
+                        <input
+                          id="checkbox-table-search-1"
+                          type="checkbox"
+                          className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                        />
+                        <label
+                          for="checkbox-table-search-1"
+                          className="sr-only"
+                        >
+                          checkbox
+                        </label>
+                      </div>
+                    </td>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      {item.id}
+                    </th>
+                    <th
+                      scope="row"
+                      className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white"
+                    >
+                      {item.first_name}
+                    </th>
+                    <td className="px-6 py-4">{item.last_name}</td>
+                    <td className="px-6 py-4">{item.email}</td>
+                    <td className="px-6 py-4">{item.gender}</td>
+                    <td className="px-6 py-4">
+                      <a
+                        href="#"
+                        className="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+                      >
+                        Edit
+                      </a>
+                    </td>
+                  </tr>
+                );
+              })}
             </tbody>
           </table>
 
@@ -89,12 +116,13 @@ export default function CustomDataTables() {
             </span>
             <ul className="inline-flex -space-x-px rtl:space-x-reverse text-sm h-8">
               <li>
-                <a
-                  href="#"
+                <button
+                  onClick={() => setCurrentPage(currentPage - 1)}
+                  disabled={currentPage === 1}
                   className="flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   Previous
-                </a>
+                </button>
               </li>
               <li>
                 <a
@@ -138,12 +166,13 @@ export default function CustomDataTables() {
                 </a>
               </li>
               <li>
-                <a
-                  href="#"
+                <button
+                  onClick={() => setCurrentPage(currentPage + 1)}
+                  disabled={currentPage === totalPages}
                   className="flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
                 >
                   Next
-                </a>
+                </button>
               </li>
             </ul>
           </nav>
