0cd7ee3c8de08e7deba3ca12c8a47b22f42e1dea
Sundze <keyonmwesundze@mail.com>
2025-02-03 10:19:49 +0100

    ...progress

--- src/components/back-office/Sidebar.jsx
+++ src/components/back-office/Sidebar.jsx
@@ -1,21 +1,90 @@
+import {
+  ExternalLink,
+  Group,
+  LayoutGrid,
+  ListOrdered,
+  Settings,
+  Slack,
+  Tractor,
+  User,
+  User2,
+  UserIcon,
+  Warehouse,
+} from "lucide-react";
 import Link from "next/link";
 
 export default function Sidebar() {
   return (
-    <div className="dark:bg-slate-700 bg-slate-100  space-y-5 w-60 h-screen dark:text-slate-100 text-slate-800 p-3 fixed left-0 top-0 shadow-md">
-      <Link href="" className="mb-6">
-        Logo
-      </Link>
+    <div className="dark:bg-slate-700 bg-slate-100  space-y-5 w-64 h-screen dark:text-slate-100 text-slate-800 fixed left-0 top-0 shadow-md">
+      <div className=" ml-8 px-6 py-4 ">
+        <Link href="" className="mb-6 text-4xl font-bold">
+          Logo
+        </Link>
+      </div>
       <div className="space-y-3 flex flex-col">
-        <Link href="">Dashboard</Link>
-        <Link href="">Catalogue</Link>
-        <Link href="">Customers</Link>
-        <Link href="">Markets</Link>
-        <Link href="">Farmers</Link>
-        <Link href="">Orders</Link>
-        <Link href="">Staff</Link>
-        <Link href="">Settings</Link>
-        <Link href="">Oline Store</Link>
+        <Link
+          href=""
+          className="flex space-x-3 px-6 py-2 border-l-4 border-green-400 "
+        >
+          <LayoutGrid />
+          <span className="">Dashboard</span>
+        </Link>
+        <Link
+          href=""
+          className="flex space-x-3 px-6 py-2 border-l-4 border-green-400 "
+        >
+          <Slack />
+          <span className="">Catalogue</span>
+        </Link>
+        <Link
+          href=""
+          className="flex space-x-3 px-6 py-2 border-l-4 border-green-400 "
+        >
+          <User />
+          <span className="">Customers</span>
+        </Link>
+        <Link
+          href=""
+          className="flex space-x-3 px-6 py-2 border-l-4 border-green-400 "
+        >
+          <Warehouse />
+          <span className="">Markets</span>
+        </Link>
+        <Link
+          href=""
+          className="flex space-x-3 px-6 py-2 border-l-4 border-green-400 "
+        >
+          <Tractor />
+          <span className="">Farmers</span>
+        </Link>
+        <Link
+          href=""
+          className="flex space-x-3 px-6 py-2 border-l-4 border-green-400 "
+        >
+          <ListOrdered />
+          <span className="">Orders</span>
+        </Link>
+        <Link
+          href=""
+          className="flex space-x-3 px-6 py-2 border-l-4 border-green-400 "
+        >
+          <User2 />
+          <span className="">Staff</span>
+        </Link>
+        <Link
+          href=""
+          className="flex space-x-3 px-6 py-2 border-l-4 border-green-400 "
+        >
+          <Settings />
+          <span className="">Settings</span>
+        </Link>
+        <Link
+          href=""
+          className="flex space-x-3 px-6 py-2 border-l-4 border-green-400 "
+        >
+          <ExternalLink />
+          <span className="">Online Stores</span>
+        </Link>
       </div>
     </div>
   );
